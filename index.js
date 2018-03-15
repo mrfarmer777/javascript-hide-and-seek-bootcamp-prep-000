53ad15995e719e73134c102ddff6291f55f1c7d9
mrfarmer777@gmail.com <mrfarmer777@gmail.com>
2018-03-15 03:48:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var getFirstSelector=function(selector){
+ var selection =document.querySelector(selector);
+ return selection;
+};
+
+var nestedTarget=function(){
+  var kids =  document.querySelector('#nested .target');
+  return kids;
+  
+};
+
+var increaseRankBy=function(n){
+  var lists = document.getElementsByClassName('ranked-list');
+  for(var i=0;i<lists.length;i++){
+    lists[i].innerHTML=parseInt(lists[i].innerHTML,10)+1;
+  }
+};
+
+var deepestChild = function(){
+  var noded = document.querySelector('#grand-node div div div div ');
+  return noded;
+
+  
+}
