3c530276b5916713b284b90d311c002cc7312687
mrfarmer777@gmail.com <mrfarmer777@gmail.com>
2018-03-15 03:45:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var getFirstSelector=function(selector){
 };
 
 var nestedTarget=function(){
-  var kids =  document.querySelector('#nested div[className[="target"');
+  var kids =  document.querySelector('#nested .target');
   return kids;
   
 };
